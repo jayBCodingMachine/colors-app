570c191b0f1f9b694031d19dce48ac9d4639eb01
Jabulani Nyawera <jaybcoding@yahoo.com>
2021-05-12 21:35:26 -0500

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
-function App() {
-	return (
-		<div>
-			<Palette palette={generatePalette(seedColors[1])} />
-		</div>
-	);
+class App extends Component {
+	render() {
+		return (
+			<Switch>
+				<Route
+					exact
+					path="/"
+					render={() => <h1>P list</h1>}
+				/>
+				<Route
+					exact
+					path="/palette/:id"
+					render={() => <h1>Palette</h1>}
+				/>
+			</Switch>
+			// <div>
+			// 	<Palette palette={generatePalette(seedColors[1])} />
+			// </div>
+		);
+	}
 }
 
 export default App;
